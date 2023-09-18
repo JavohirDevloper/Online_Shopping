583fc01c075a22193d68a5072fbc0762fc23d0c6
JavohirDevloper <muhammadiyevj72@gmail.com>
2023-09-18 17:35:05 +0500

    online shopping for frontend src code

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1 className="text-center">Rights Reserved &copy; JavohirDev</h1>
+      <p className="text-center mt-3">
+        <Link to="/contact">Contact</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
