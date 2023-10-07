5d970b4abb73cccac2f2b3b09600e012ef55db42
JavohirDevloper <muhammadiyevj72@gmail.com>
2023-10-07 18:13:31 +0500

    commit

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <h1 className="text-center">Rights Reserved &copy; JavohirDev</h1>
-      <p className="text-center mt-3">
+      <p className="text-center mt-1">
         <Link to="/contact">Contact</Link>
       </p>
     </div>
